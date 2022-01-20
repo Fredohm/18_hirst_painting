156ea2d4b2a3d9a1c88a1db9d9dd787699fdc7d4
Fredohm <fredwaucquez@hotmail.fr>
2022-01-20 09:21:51 +0100

    user colorgram package to extract rgb colors of image

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import colorgram
 
+selected_colors = colorgram.extract('hirst-white-paper.jpg', 20)
+rgb_tuples = []
+for selected_color in selected_colors:
+    rgb_tuples.append(tuple(selected_color.rgb))
+print(rgb_tuples)
